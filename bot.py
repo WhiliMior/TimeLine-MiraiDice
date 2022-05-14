6df596397e2a94103d8f241fd24c128558b6f3a0
WhiliMior <whilimior@gmail.com>
2022-05-14 18:26:18 -0400

    Delete test part

--- bot.py
+++ bot.py
@@ -19,13 +19,6 @@ qq = int(get_value('qq'))
 verify_key = get_value('verifyKey')
 port = int(get_value('port'))
 
-
-@miraicle.MiraiCode
-def friend_invite(bot: miraicle.Mirai, msg: miraicle.FriendMessage, event: miraicle.MiraiCode):
-    var = event
-    print(var, '这就是街舞')
-
-
 bot = miraicle.Mirai(qq=qq, verify_key=verify_key, port=port)
 bot.set_filter(miraicle.GroupSwitchFilter(r'config\group_switch.json'))
 bot.run()
